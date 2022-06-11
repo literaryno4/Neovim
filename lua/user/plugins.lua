ab2dda5e616dc78ed7df7bbe7803df156004b9ca
literaryno4 <m18260037123@163.com>
2022-06-11 21:54:55 +0800

    remove coc

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -95,10 +95,6 @@ return packer.startup(function(use)
   -- Git
   use "lewis6991/gitsigns.nvim"
 
-  -- coc
-  use {'neoclide/coc.nvim', branch = 'release'}
-
-
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
 --   if PACKER_BOOTSTRAP then
