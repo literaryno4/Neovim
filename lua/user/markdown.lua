974b81d2fa1f3747743edd43b7131a6517055f55
literaryno4 <m18260037123@163.com>
2022-07-17 17:57:08 +0800

    new branch for terminal

--- lua/user/markdown.lua
+++
@@ -1,7 +0,0 @@
-vim.g["mkdp_port"] = '8555'
-vim.g["vim_markdown_math"] = 1
--- vim.g["mkdp_auto_start"] = 1
-vim.g["mkdp_auto_close"] = 1
-vim.g["mkdp_open_to_the_world"] = 1
-vim.g["mkdp_echo_preview_url"] = 1
-vim.g["vmt_auto_update_on_save"] = 1
--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -82,15 +82,6 @@ return packer.startup(function(use)
   use "tamago324/nlsp-settings.nvim" -- language server settings defined in json for
   use "jose-elias-alvarez/null-ls.nvim" -- for formatters and linters
 
-  -- markdown
-  use({
-    "iamcco/markdown-preview.nvim",
-    run = function() vim.fn["mkdp#util#install"]() end,
-  })
-
-  use 'mzlogin/vim-markdown-toc'
-  use 'plasticboy/vim-markdown'
-
   -- Telescope
   use "nvim-telescope/telescope.nvim"
 
