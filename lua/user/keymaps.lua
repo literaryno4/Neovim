eb11b889395852434816452ae41d895f17f1d770
literaryno4 <m18260037123@163.com>
2022-08-03 10:20:01 +0800

    update

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -32,8 +32,8 @@ keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
 keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 
 -- Navigate buffers
-keymap("n", "<S-l>", ":tabn<CR>", opts)
-keymap("n", "<S-h>", ":tabp<CR>", opts)
+keymap("n", "<S-l>", ":bnext<CR>", opts)
+keymap("n", "<S-h>", ":bprevious<CR>", opts)
 
 -- Move text up and down
 keymap("n", "<A-j>", "<Esc>:m .+1<CR>==gi", opts)
@@ -64,7 +64,7 @@ keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
 keymap("n", "<leader>n", ":NERDTreeFocus<CR>", opts)
 keymap("n", "<C-n>", ":NERDTree<CR>", opts)
 keymap("n", "<C-t>", ":NERDTreeToggle<CR>", opts)
-keymap("n", "<C-f>", ":NERDTreeFind<CR>", opts)
+-- keymap("n", "<C-f>", ":NERDTreeFind<CR>", opts)
 
 -- Terminal --
 -- Better terminal navigation
